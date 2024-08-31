77bf73d33aa2abfe1216255e7f3813f4f741da09
yr2610 <shin.nakaji@nifty.com>
2024-09-01 05:01:52 +0900

    パスワード生成プログラム作成

---
+++ UserPassGen/PlaceholderTextBox.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class PlaceholderTextBox : TextBox
+{
+    private string placeholderText;
+
+    [Category("Placeholder")]
+    [Description("The placeholder text that is displayed when the textbox is empty.")]
+    public string PlaceholderText
+    {
+        get { return placeholderText; }
+        set { placeholderText = value; Invalidate(); }
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        if (string.IsNullOrEmpty(this.Text) && !string.IsNullOrEmpty(this.PlaceholderText))
+        {
+            using (Brush brush = new SolidBrush(Color.Gray))
+            {
+                e.Graphics.DrawString(this.PlaceholderText, this.Font, brush, new PointF(0, 0));
+            }
+        }
+    }
+
+    protected override void OnGotFocus(EventArgs e)
+    {
+        base.OnGotFocus(e);
+        this.Invalidate();
+    }
+
+    protected override void OnLostFocus(EventArgs e)
+    {
+        base.OnLostFocus(e);
+        this.Invalidate();
+    }
+}
--- UserPassGen/Program.cs
+++ UserPassGen/Program.cs
@@ -1,22 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
 using System.Windows.Forms;
 
-namespace UserPassGen
+public class PasswordForm : Form
 {
-    static class Program
+    private PlaceholderTextBox userNameTextBox;
+    private PlaceholderTextBox projectNameTextBox;
+    private DateTimePicker expirationDatePicker;
+    private Button generateButton;
+    private TextBox passwordTextBox;
+    private Button copyButton;
+
+    public PasswordForm()
     {
-        /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
-        /// </summary>
-        [STAThread]
-        static void Main()
+        this.Text = "UserPassGen"; // ウィンドウの名称にプロジェクト名を表示
+        this.Size = new System.Drawing.Size(450, 400); // ウィンドウの初期サイズを大きく設定
+
+        projectNameTextBox = new PlaceholderTextBox { PlaceholderText = "プロジェクト名を入力", Top = 10, Left = 10, Width = 400, Font = new System.Drawing.Font("Microsoft Sans Serif", 16F) };
+        userNameTextBox = new PlaceholderTextBox { PlaceholderText = "ユーザー名を入力", Top = 60, Left = 10, Width = 400, Font = new System.Drawing.Font("Microsoft Sans Serif", 16F) };
+        expirationDatePicker = new DateTimePicker { Top = 110, Left = 10, Width = 400, Font = new System.Drawing.Font("Microsoft Sans Serif", 16F) };
+        generateButton = new Button { Text = "パスワード生成", Top = 160, Left = 10, Width = 400, Height = 50, Font = new System.Drawing.Font("Microsoft Sans Serif", 16F) };
+        passwordTextBox = new TextBox { Top = 220, Left = 10, Width = 400, ReadOnly = true, Font = new System.Drawing.Font("Microsoft Sans Serif", 16F) };
+        copyButton = new Button { Text = "クリップボードにコピー", Top = 280, Left = 10, Width = 400, Height = 50, Font = new System.Drawing.Font("Microsoft Sans Serif", 16F) };
+
+        generateButton.Click += GenerateButton_Click;
+        copyButton.Click += CopyButton_Click;
+
+        Controls.Add(projectNameTextBox);
+        Controls.Add(userNameTextBox);
+        Controls.Add(expirationDatePicker);
+        Controls.Add(generateButton);
+        Controls.Add(passwordTextBox);
+        Controls.Add(copyButton);
+    }
+
+    private void GenerateButton_Click(object sender, EventArgs e)
+    {
+        string projectName = projectNameTextBox.Text;
+        string userName = userNameTextBox.Text;
+        DateTime expirationDate = expirationDatePicker.Value;
+
+        string password = GeneratePassword(projectName, userName, expirationDate);
+        passwordTextBox.Text = password;
+    }
+
+    private void CopyButton_Click(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(passwordTextBox.Text))
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Clipboard.SetText(passwordTextBox.Text);
+            MessageBox.Show("パスワードがクリップボードにコピーされました。", "コピー完了", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+        else
+        {
+            MessageBox.Show("コピーするパスワードがありません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    private string GeneratePassword(string projectName, string userName, DateTime expirationDate)
+    {
+        DateTime baseDate = new DateTime(1970, 1, 1);
+        int daysSinceBase = (expirationDate - baseDate).Days;
+        string hexDays = daysSinceBase.ToString("x4"); // 16進数の4文字で小文字表示
+
+        string input = projectName + userName + hexDays;
+
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+            string hashString = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            return hexDays + hashString.Substring(0, 8); // 有効期限とハッシュ値の一部を結合
+        }
+    }
+}
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new PasswordForm());
     }
 }
